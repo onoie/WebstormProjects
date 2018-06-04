7871326da033315542bead13b70ad1fbef58419b
onoie <onoie3@gmail.com>
2018-06-04 14:20:59 +0900

    Update index.js

--- electron/index.js
+++ electron/index.js
@@ -1,31 +1,25 @@
 'use strict';
 
-// アプリケーションをコントロールするモジュール
 var electron = require('electron');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
+var mainWindow = null;
 
-// メインウィンドウはGCされないようにグローバル宣言
-let mainWindow;
-
-// 全てのウィンドウが閉じたら終了
 app.on('window-all-closed', function() {
-  if (process.platform != 'darwin') {
+  if (process.platform != 'darwin')
     app.quit();
-  }
 });
 
-// Electronの初期化完了後に実行
 app.on('ready', function() {
-  // メイン画面の表示
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600
-  });
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-  //ウィンドウが閉じられたらアプリも終了
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
+    mainWindow = new BrowserWindow({
+        // transparent: true,
+        // frame: false,
+        width: 800,
+        height: 600,
+        'node-integration': false,
+    });
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
 });
